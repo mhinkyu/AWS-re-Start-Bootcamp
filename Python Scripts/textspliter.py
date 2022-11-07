7282ae3ff1236720883a6965aa929d0f687d8e23
Mhinkyu <mhinkyuk@gmail.com>
2022-11-07 17:13:22 +0200

    textspliter script added

---
+++ Python Scripts/textspliter.py
@@ -0,0 +1,10 @@
+import re # regular expression
+
+
+def main():
+    text =  """Python is powerful... and fast
+plays well with others
+runs everywhere
+is friendly & easy to learn
+is Open
+These are some of the reasons people who use Python would rather not use anything else"""
