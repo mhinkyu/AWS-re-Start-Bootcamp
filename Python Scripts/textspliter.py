69b5fd97e3b27b4304ed64393c83e7fdfacf2903
Mhinkyu <mhinkyuk@gmail.com>
2022-11-07 17:16:11 +0200

    function main added

--- Python Scripts/textspliter.py
+++ Python Scripts/textspliter.py
@@ -8,3 +8,14 @@ runs everywhere
 is friendly & easy to learn
 is Open
 These are some of the reasons people who use Python would rather not use anything else"""
+
+    text = text.lower()
+    text = re.sub(r'[^a-z0-9\s]', '', text)
+    words = list(text.split())
+    ls = []
+    for i in words:
+        if i not in ls:
+            ls.append(i)
+    print(ls)
+main()
+
